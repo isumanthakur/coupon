3e5889bbd33b5c6f375c75b458324fa21405f771
iSumanThakur436 <tsuman436@gmail.com>
2024-07-23 02:59:55 +0530

    version 1.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,47 @@
-import { useState } from 'react'
-import image from './assets/i1.png'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import './App.css';
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from './components';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  const theme = useSelector((state) => state.theme.themeMode);
 
-  return (
-    <>
-      <img src={image} alt="" />
-    </>
-  )
+  useEffect(() => {
+      if (theme === 'dark') {
+          document.documentElement.classList.add('dark');
+      } else {
+          document.documentElement.classList.remove('dark');
+      }
+  }, [theme]);
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({ userData }));
+      } else {
+        dispatch(logout());
+      }
+    })
+    .finally(() => setLoading(false));
+  }, [dispatch]);
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <Header />
+      <div className='flex-grow'>
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
